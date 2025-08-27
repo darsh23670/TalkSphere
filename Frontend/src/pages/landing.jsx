541d147e5ae01419e8737eccffc81186a1d33a24
Darshan M <dd6785913@gmail.com>
2025-08-27 11:57:42 +0530

    Changed Tile

--- Frontend/src/pages/landing.jsx
+++ Frontend/src/pages/landing.jsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
     <div className="landingPageContainer">
       <nav>
         <div className="navHeader">
-          <h2>Apna Video Call</h2>
+          <h2>TalkSphere</h2>
         </div>
         <div className="navlist">
           <p
@@ -43,7 +43,7 @@ export default function LandingPage() {
             Ones
           </h1>
 
-          <p>Cover a distance by Apna Video Call</p>
+          <p>Cover a distance by TalkSphere</p>
           <div role="button">
             <Link to={"/auth"}>Get Started</Link>
           </div>
